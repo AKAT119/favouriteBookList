e468727d31dbdf413de01f79522b8f15982388b4
Azin Katiraee <azinnash@yahoo.com>
2022-11-01 14:42:32 -0400

    update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,5 @@ module.exports = {
   //local MongoDB deployment ->
   //"URI": "mongodb://127.0.0.1/books229"
 
-  "URI": "mongodb+srv://aizakat:PENl2IqnVhbCUYo9@cluster1.hlcgbi3.mongodb.net/?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://aizakat:PENl2IqnVhbCUYo9@cluster1.hlcgbi3.mongodb.net/books?retryWrites=true&w=majority"
 };
