da943d80d87dbd5aef3acb0132ea26c9f0c883fd
Azin Katiraee <azinnash@yahoo.com>
2022-11-01 14:01:32 -0400

    update db.js with atlas URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://127.0.0.1/books229"
+  //"URI": "mongodb://127.0.0.1/books229"
+
+  "URI": "mongodb+srv://aizakat:PENl2IqnVhbCUYo9@cluster1.hlcgbi3.mongodb.net/?retryWrites=true&w=majority"
 };
